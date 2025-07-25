00eab0aab39d43c46eca05f7b6456978464e0aa4
KroNton <kyrlosfekry@gmail.com>
2025-07-25 21:23:44 +0300

    Refactor SimpleNode constructor for improved readability and maintainability

--- module1/my_first_cpp_pkg/src/simple_node.cpp
+++ module1/my_first_cpp_pkg/src/simple_node.cpp
@@ -6,24 +6,24 @@ that outputs a startup message when initialized. */
 
 class SimpleNode : public rclcpp::Node
 {
-public:
-  SimpleNode()
-  : Node("simple_node")
-  {
-    RCLCPP_INFO(this->get_logger(), "Simple node Is starting up...");
 
-  }
-    ~SimpleNode()
+  public:
+    SimpleNode(): Node("simple_node")// member initializer list
     {
-        RCLCPP_INFO(this->get_logger(), "Simple node Is shutting down...");
+      RCLCPP_INFO(this->get_logger(), "Simple node Is starting up...");
+
+      //RCLCPP_INFO → Logs a message at the "INFO" level (general operational messages).
+      //this->get_logger() → Gets the logger associated with this node (includes the node name in logs).
     }
+
 };
 
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<SimpleNode>());
+  auto node = std::make_shared<SimpleNode>();  // Heap allocation + shared_ptr
+  rclcpp::spin(node);  
   rclcpp::shutdown();
   return 0;
 }
