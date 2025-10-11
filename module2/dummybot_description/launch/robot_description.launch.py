26fee056a1adb9b8c20c609993f3658c0507655f
KroNton <kyrlosfekry@gmail.com>
2025-10-12 00:57:35 +0300

    Add Gazebo simulation configuration and update URDF to include Gazebo file

---
+++ module2/dummybot_description/launch/robot_description.launch.py
@@ -0,0 +1,33 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+
+
+    pkg_share = get_package_share_directory('dummybot_description')
+
+    urdf_path = os.path.join(pkg_share,'urdf','dummybot.xacro')
+
+    robot_description= ParameterValue(
+
+        Command(['xacro ', urdf_path]),
+        value_type=str
+    )
+
+    robot_state_publisher_node= Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description':robot_description}]
+    )
+
+
+
+    return LaunchDescription([
+
+        robot_state_publisher_node,
+    ])
